dfd0d9e43c879269b459762de033f7f685045659
Allan Ndung'u Kanja <linux7506@gmail.com>
2025-04-24 22:08:11 -0700

    added files

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(%53balxhz++@7co52*^=6h=bb*y173o3q$_b4h48l8sff_o-)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static" ]
+STATICFILES_DIRS = [BASE_DIR, "static" ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
