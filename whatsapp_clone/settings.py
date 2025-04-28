604e929ab28cc51822974fee39be2011184e0c0a
Buggystepper <linux7506@gmail.com>
2025-04-28 21:35:44 +0300

    Update settings.py

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(%53balxhz++@7co52*^=6h=bb*y173o3q$_b4h48l8sff_o-)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chat-zmmf.onrender.com']
 
 
 # Application definition
