72dab81d51828779faacb720a54acb1faa58ae9e
Buggystepper <linux7506@gmail.com>
2025-04-29 00:18:11 +0300

    Update settings.py

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -139,11 +139,8 @@ ASGI_APPLICATION = 'whatsapp_clone.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
-        },
-    },
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
 }
 
 
